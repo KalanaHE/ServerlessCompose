d5aa6617da8181981fdade63966d472031255425
Kalana Hettiarachchi <kalana@orelit.com>
2023-09-12 11:27:42 +0530

    sequelize intergration

--- services/appointment-service/handler.js
+++ services/appointment-service/handler.js
@@ -1,17 +1,11 @@
 "use strict";
 
-const User = require("../../models/appointmentdb/user");
-
 module.exports.getData = async (event, context) => {
-  const user = await User.findOne({
-    where: { id: 1 },
-  });
-
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "appointment-service: Test Data",
-      data: user,
+
       // You can replace this with actual data from your MySQL database
     }),
   };
--- services/doctor-service/handler.js
+++ services/doctor-service/handler.js
@@ -1,10 +1,17 @@
 "use strict";
 
+const User = require("../../models/appointmentdb/user");
+
 module.exports.getData = async (event, context) => {
+  const user = await User.findOne({
+    where: { id: 1 },
+  });
+
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "doctor-service: Test Data",
+      data: user,
       // You can replace this with actual data from your MySQL database
     }),
   };
